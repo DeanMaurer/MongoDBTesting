4c7eacef67161dfacd8099154422f99bffad4d9b
Dean Maurer <dmaurer@eidpassport.com>
2015-11-24 09:14:27 -0800

    Initial commit for the TestMongoDB project

---
+++ TestMongoDB/TestMongoDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TestMongoDB
+{
+    class Program
+    {
+
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+        
+
+        static void Main(string[] args)
+        {
+            _client = new MongoClient();
+            _database = _client.GetDatabase("test");
+
+        }
+    }
+}
